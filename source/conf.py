45d94f510864f81ab0bcd1288e9ac92f15055395
Alfonso Vásquez <avasquez@rivetlogic.com>
2015-12-16 20:17:25 -0600

    * Added configuration parameter to make java the default highlighted language. * Added the Groovy Development Page.

--- source/conf.py
+++ source/conf.py
@@ -366,3 +366,6 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# The default source code language for literals
+highlight_language = 'java'
