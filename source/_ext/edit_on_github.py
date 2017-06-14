19593a536c045587f193b55f99884f9460c11af6
Sumer Jabri <sjabri@rivetlogic.com>
2017-06-14 16:30:26 -0400

    Small changes/tweaks to try to get Edit On  Github working.

--- source/_ext/edit_on_github.py
+++ source/_ext/edit_on_github.py
@@ -29,19 +29,19 @@ def html_page_context(app, pagename, templatename, context, doctree):
         warnings.warn("edit_on_github_project not specified")
         return
 
-    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
-    show_url = get_github_url(app, 'blob', path)
-    edit_url = get_github_url(app, 'edit', path)
-
-    context['show_on_github_url'] = show_url
-    context['edit_on_github_url'] = edit_url
-
     # For sphinx_rtd_theme.
     context['display_github'] = True
     context['github_user'] = app.config.edit_on_github_project.split('/')[0]
     context['github_version'] = app.config.edit_on_github_branch + '/' + app.config.edit_on_github_base_folder + '/'
     context['github_repo'] = app.config.edit_on_github_project.split('/')[1]
     context['source_suffix'] = app.config.source_suffix[0]
+
+    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
+    show_url = get_github_url(app, 'blob', path)
+    edit_url = get_github_url(app, 'edit', path)
+
+    context['show_on_github_url'] = show_url
+    context['edit_on_github_url'] = edit_url
 
 def setup(app):
     app.add_config_value('edit_on_github_project', '', True)
