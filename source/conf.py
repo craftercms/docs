c81faf59bfc4900082d14fbeeae1f84c47e46af7
Roy Art <royart@me.com>
2017-08-28 12:30:30 +0200

    Switching to custom Crafter CMS Sphinx theme

--- source/conf.py
+++ source/conf.py
@@ -122,15 +122,26 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-html_theme = 'sphinx_rtd_theme'
 #html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
+
+# https://github.com/rart/craftercms-sphinx-theme
+html_theme = 'craftercms_sphinx_theme'
 
 html_show_sphinx = False
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "analytics_id": "UA-40677244-9",
+    "topbar_items": [
+        {"text":"CrafterCMS Site", "link":"//craftercms.org"}
+    ]
+}
+
+# Adding additional css files...
+# html_context = { "extra_css_files": ["_static/custom.css"] }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
