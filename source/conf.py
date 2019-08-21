9f844cd4e098124f66c71b4d1ec15545dfe8db60
Vita Alhambra <vita.alhambra@craftersoftware.com>
2019-08-21 15:15:29 -0400

    Update swaggerhub api version

--- source/conf.py
+++ source/conf.py
@@ -415,7 +415,7 @@ javadoc_version = '3.1.0'
 
 # swagger base and version
 studio_swagger_base = 'https://app.swaggerhub.com/apis/craftercms/studio/'
-studio_swagger_version = '3.1.1.15'
+studio_swagger_version = '3.1.1.16'
 
 # Shorten external links
 extlinks = {'javadoc_base_url': (javadoc_base + javadoc_version + '/%s', None ),
