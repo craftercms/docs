e26d663e3d80305ac151d9a722a7ce0814febe1d
Sumer Jabri <sumer@craftercms.org>
2019-12-06 10:56:12 -0500

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -411,7 +411,7 @@ rst_epilog = """
 .. |enterpriseOnly| image:: /_static/images/ee-only-badge.svg
                       :alt: Enterprise only feature
                       :class: ee-only-badge
-                      :target: http://www.craftersoftware.com
+                      :target: https://craftersoftware.com
 """
 
 # Javadoc home and version
