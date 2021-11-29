16a8e8a64a8435d265129ffd84ca7dfaf0adcfca
jvega190 <josevega19@gmail.com>
2021-11-29 13:33:43 -0600

    [3984] Add _Since_ and _Last Updated_ tags to all pages (#1377)
    * Add styles to since and last updated tags - #3984
    
    * Adjust font-size - #3984
    
    * Add extension to allow adding since and last updated tags inline - #3984
    
    * Update directive to render generic tags - #3984
    
    * Indentation
    
    * Update layout.html
    
    Co-authored-by: Sumer Jabri <sumer@craftercms.org>

--- source/conf.py
+++ source/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'edit_on_github',
     'sphinx.ext.extlinks',
+    'version_tag'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
