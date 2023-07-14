acb4ef0cea2e0839aa7143fc6cfdf6daed9fdbd7
Vita Alhambra <vita.alhambra@craftersoftware.com>
2023-07-14 06:32:40 -0400

    Add release notes for 4.0.7

--- source/conf.py
+++ source/conf.py
@@ -29,7 +29,7 @@ author = u'CrafterCMS'
 # The short X.Y version.
 version = u'4.0'
 # The full version, including alpha/beta/rc tags.
-release = u'4.0.6'
+release = u'4.0.7'
 
 
 # -- General configuration ---------------------------------------------------
@@ -240,7 +240,7 @@ rst_epilog = """
 
 # Javadoc home and version
 javadoc_base = 'http://javadoc.craftercms.org/'
-javadoc_version = '4.0.6'
+javadoc_version = '4.0.7'
 
 # Shorten external links
 extlinks = {'javadoc_base_url': (javadoc_base + javadoc_version + '/%s', None ),
