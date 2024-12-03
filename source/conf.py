caf66ec15da70e7acecd20812bc53971abb435dc
Sumer Jabri <sumer.jabri@craftercms.org>
2024-12-03 17:15:23 -0500

    Removed language from the base URL the replaced it with 'docs'

--- source/conf.py
+++ source/conf.py
@@ -75,7 +75,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'en'
+#language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -139,7 +139,7 @@ html_favicon = "_static/images/favicon.ico"
 # Add versions for the bottom menu
 html_context = {
     'versions': ['4.1', '4.0', '3.1'],
-    'docsUrl': 'https://docs.craftercms.org'
+    'docsUrl': '/docs'
 }
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -244,7 +244,7 @@ javadoc_version = '4.0.6'
 
 # Shorten external links
 extlinks = {'javadoc_base_url': (javadoc_base + javadoc_version + '/%s', None ),
-            'base_url': ('/' + language + '/' + version + '/%s', None)
+            'base_url': ('/docs/' + version + '/%s', None)
             }
 
 
