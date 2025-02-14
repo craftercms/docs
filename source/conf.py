0f7964d87863d12277ca0a75be5caf56f671efc0
Vita Alhambra <vita.alhambra@craftersoftware.com>
2025-02-14 16:33:12 -0500

    [3.1] Update version picker

--- source/conf.py
+++ source/conf.py
@@ -137,7 +137,7 @@ html_favicon = "_static/images/favicon.ico"
 
 # Add versions for the bottom menu
 html_context = {
-    'versions': ['4.2', '4.1', '4.0', '3.1'],
+    'versions': ['4', '4.1', '4.0', '3.1'],
     'docsUrl': '/docs'
 }
 
